e4ae6ab74e87cd6c390ab6fbf5d0b6da138e9808
Jartholdy <96985410+Jartholdy@users.noreply.github.com>
2024-09-10 23:02:19 +0200

    Update _Main.lua

--- Preferences/_Main.lua
+++ Preferences/_Main.lua
@@ -1,103 +1,68 @@
---------------------------------------------------------------------------------
----- AddOn Namespace
---------------------------------------------------------------------------------
-
+-- ----------------------------------------------------------------------------
+-- AddOn Namespace
+-- ----------------------------------------------------------------------------
 local AddOnFolderName = ... ---@type string
-local private = select(2, ...) ---@type PrivateNamespace
+local private = select(2, ...) ---@class PrivateNamespace
 
---------------------------------------------------------------------------------
----- Initialization
---------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
+-- Initialization
+-- ----------------------------------------------------------------------------
+local metaVersion = C_AddOns.GetAddOnMetadata(AddOnFolderName, "Version")
+local isDevelopmentVersion = false
+local isAlphaVersion = false
 
----@return string
-local function GetBuildVersion()
-    local metaVersion = GetAddOnMetadata(AddOnFolderName, "Version")
-    local isDevelopmentVersion = false
-    local isAlphaVersion = false
+--[==[@debug@
+isDevelopmentVersion = true
+--@end-debug@]==]
 
-    --@debug@
-    isDevelopmentVersion = true
-    --@end-debug@
+--[=[@alpha@
+isAlphaVersion = true
+--@end-alpha@]=]
 
-    if isDevelopmentVersion then
-        return "Development Version"
-    end
+local buildVersion = isDevelopmentVersion and "Development Version"
+    or (isAlphaVersion and metaVersion .. "-Alpha")
+    or metaVersion
 
-    --@alpha@
-    isAlphaVersion = true
-    --@end-alpha@
-
-    if isAlphaVersion then
-        return ("%s-Alpha"):format(metaVersion)
-    end
-
-    return metaVersion or UNKNOWN
-end
-
---------------------------------------------------------------------------------
----- Preferences
---------------------------------------------------------------------------------
-
----@type AceConfig.OptionsTable
+-- ----------------------------------------------------------------------------
+-- Preferences
+-- ----------------------------------------------------------------------------
+---@type table
 local Options
 
 ---@class Preferences
 ---@field DataObject Preferences.DataObject
----@field DefaultValues AceDB.Schema
 ---@field Tooltip Preferences.Tooltip
 ---@field OptionsFrame Frame
-local Preferences = private.Preferences
-
-Preferences.DataObject = {}
-Preferences.DefaultValues = {
-    global = {
-        ZoneData = {}, -- Populated during travel.
+local Preferences = {
+    DefaultValues = {
+        global = {
+            ZoneData = {}, -- Populated during travel.
+        },
     },
+    GetOptions = function()
+        if not Options then
+            Options = {
+                name = ("%s - %s"):format(AddOnFolderName, buildVersion),
+                type = "group",
+                childGroups = "tab",
+                args = {
+                    dataObject = private.Preferences.DataObject.GetOptions(),
+                    tooltip = private.Preferences.Tooltip.GetOptions(),
+                },
+            }
+        end
+
+        return Options
+    end,
+    ---@param self Preferences
+    InitializeDatabase = function(self)
+        return LibStub("AceDB-3.0"):New(AddOnFolderName .. "DB", self.DefaultValues, true).global
+    end,
+    ---@param self Preferences
+    SetupOptions = function(self)
+        LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable(AddOnFolderName, self.GetOptions)
+        self.OptionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(AddOnFolderName)
+    end,
 }
 
-Preferences.Tooltip = {}
-
----@return AceConfig.OptionsTable
-function Preferences:GetOptions()
-    if not Options then
-        Options = {
-            childGroups = "tree",
-            name = ("%s - %s"):format(AddOnFolderName, GetBuildVersion()),
-            type = "group",
-            args = {
-                DataObject = Preferences.DataObject:GetOptions(),
-                Tooltip = Preferences.Tooltip:GetOptions(),
-            },
-        }
-    end
-
-    return Options
-end
-
-function Preferences:InitializeDatabase()
-    return LibStub("AceDB-3.0"):New(("%sDB"):format(AddOnFolderName), self.DefaultValues, true).global
-end
-
-function Preferences:SetupOptions()
-    LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable(AddOnFolderName, self.GetOptions)
-
-    self.OptionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(AddOnFolderName)
-end
-
-function Preferences:ToggleOptionsVisibility()
-    local AceConfigDialog = LibStub("AceConfigDialog-3.0")
-
-    if AceConfigDialog.OpenFrames[AddOnFolderName] then
-        AceConfigDialog:Close(AddOnFolderName)
-    else
-        AceConfigDialog:Open(AddOnFolderName)
-        AceConfigDialog:SelectGroup(AddOnFolderName, "Tooltip", "General")
-    end
-end
-
---------------------------------------------------------------------------------
----- Type Augmentation
---------------------------------------------------------------------------------
-
----@class AceConfigDialog-3.0
----@field OpenFrames table<string, unknown>
+private.Preferences = Preferences
